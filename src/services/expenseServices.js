b68ab6bb63a96d803cfff076b428d4fba2cf4b3b
voidirl <rishavrajput204@gmail.com>
2026-03-12 11:11:29 +0530

    add expense service API calls

---
+++ src/services/expenseServices.js
@@ -0,0 +1,35 @@
+const BASE_URL = "http://localhost:8080/api/expenses";
+
+export const getAllExpenses = async () => {
+    const res = await fetch(BASE_URL);
+    return res.json();
+};
+
+export const addExpense = async (expense) => {
+    const res = await fetch(BASE_URL,{
+        method:"POST",
+        headers: {"Content-Type":"application/json"},
+        body:JSON.stringify(expense),
+    });
+    return res.json();
+};
+
+export const updateExpense = async(id,expense) => {
+    const res = await fetch('${BASE_URL}/${id}',{
+        method:"PUT",
+        headers: {"Content-Type":"application/json"},
+        body:JSON.stringify(expense),
+    });
+    return res.json();
+};
+
+export const deleteExpense = async(id) => {
+   await fetch ('${BASE_URL}/{id}',{
+    method:"DELETE"
+   });
+};
+
+export const getTotalSum = async () => {
+    const res = await fetch('${BASE_URL}/total');
+    return res.json();
+} ;
