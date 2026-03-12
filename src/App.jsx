4e81120c33c6278c3b0a8164a25a70e64bff6eb9
voidirl <rishavrajput204@gmail.com>
2026-03-12 10:47:58 +0530

    initial setup: vite + react + tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App(){
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justufy-center">
+      <h1 className="text-4x1 font-bold text-blue-600">Expense Tracker</h1>
+    </div>
+  )
+}
+
+export default App
